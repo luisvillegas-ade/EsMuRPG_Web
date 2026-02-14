ee699496c8b64b47eb77dc24b35b2d621a0bf884
Luis Villegas <luisvillegas.ade@gmail.com>
2026-02-14 17:03:08 -0300

    biblioteca 2

--- js/plugins/CargadorDesafios.js
+++ js/plugins/CargadorDesafios.js
@@ -1,8 +1,8 @@
-window.abrirDesafio = function(nombreCarpeta) {
+window.abrirDesafio = function (nombreCarpeta) {
     var iframe = document.createElement('iframe');
     iframe.id = 'pantalla_minijuego';
     iframe.src = 'desafios/' + nombreCarpeta + '/index.html';
-    
+
     // ESTILO CENTRADO
     iframe.style.position = 'fixed';
     iframe.style.top = '50%';
@@ -13,28 +13,28 @@ window.abrirDesafio = function(nombreCarpeta) {
     iframe.style.border = '4px solid #d4af37';
     iframe.style.zIndex = '10000';
     iframe.style.backgroundColor = '#0d1117';
-    
+
     document.body.appendChild(iframe);
 
     window.addEventListener('message', function receptor(event) {
         var pantalla = document.getElementById('pantalla_minijuego');
-        
+
         // --- CASO: GANASTE ---
         if (event.data.tipo === "FIN_DESAFIO") {
-            if(pantalla) document.body.removeChild(pantalla);
+            if (pantalla) document.body.removeChild(pantalla);
             window.removeEventListener('message', receptor);
-            
+
             // Lógica de Interruptores según el juego
             // Si el juego no manda ID, asumimos que es el primero (ritmo_01)
             if (!event.data.id || event.data.id === "ritmo_01") {
-                 $gameSwitches.setValue(1, true); // Interruptor 1: Negra
-            } 
-            else if (event.data.id === "chacarera") {
-                 $gameSwitches.setValue(2, true); // Interruptor 2: Chacarera
+                $gameSwitches.setValue(1, true); // Interruptor 1: Negra
             }
-              // --- AGREGAR ESTO ---
+            else if (event.data.id === "chacarera") {
+                $gameSwitches.setValue(2, true); // Interruptor 2: Chacarera
+            }
+            // --- AGREGAR ESTO ---
             else if (event.data.id === "vals") {
-                 $gameSwitches.setValue(3, true); // Vals (Interruptor 3)
+                $gameSwitches.setValue(3, true); // Vals (Interruptor 3)
             }
 
             $gameMessage.add("¡Desafío superado! Puntaje: " + event.data.puntaje);
@@ -42,7 +42,7 @@ window.abrirDesafio = function(nombreCarpeta) {
 
         // --- CASO: CANCELASTE ---
         if (event.data.tipo === "CANCELAR_DESAFIO") {
-            if(pantalla) document.body.removeChild(pantalla);
+            if (pantalla) document.body.removeChild(pantalla);
             window.removeEventListener('message', receptor);
             $gameMessage.add("No te preocupes, podés volver a intentarlo cuando quieras.");
         }
